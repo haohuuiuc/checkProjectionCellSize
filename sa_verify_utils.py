2505034002200fab31dc55fb9bcbb4eaf5b65f6f
Hao Hu <h.hu@esri.com>
2018-09-04 16:53:08 -0700

    same commit on standalone script

--- sa_verify_utils.py
+++ sa_verify_utils.py
@@ -8,19 +8,25 @@ import arcpy
 import math
 
 
-def extent_to_polygon(extent, input_spatial_reference=None):
+def extent_to_polygon(extent, input_spatial_reference=None, in_ras=None):
     """
     Convert extent to arcpy.Polygon object
     :type extent: arcpy.Extent
     :type input_spatial_reference: arcpy.SpatialReference
+    :type in_ras: arcpy.sa.Raster
     :rtype arcpy.Polygon
     """
+    if extent.spatialReference:
+        input_spatial_reference = extent.spatialReference
     pnt1 = arcpy.Point(extent.XMin, extent.YMin)
     pnt2 = arcpy.Point(extent.XMin, extent.YMax)
     pnt3 = arcpy.Point(extent.XMax, extent.YMax)
     pnt4 = arcpy.Point(extent.XMax, extent.YMin)
     array = arcpy.Array([pnt1, pnt2, pnt3, pnt4])
-    return arcpy.Polygon(array, input_spatial_reference)
+    polygon = arcpy.Polygon(array, input_spatial_reference)
+    if in_ras is not None:
+        polygon = polygon.densify("DISTANCE", in_ras.meanWidth, in_ras.meanWidth)
+    return polygon
 
 
 def get_extent(input_data, output_spatial_reference=None):
@@ -49,10 +55,10 @@ def calculate_output_extent_intersect(input_list, output_spatial_reference=None)
     :rtype arcpy.Polygon
     """
     extent_list = [get_extent(i, output_spatial_reference) for i in input_list]
-    return extent_to_polygon(arcpy.Extent(min(map(lambda x: x.XMax, extent_list)),
-                                          max(map(lambda x: x.XMin, extent_list)),
-                                          min(map(lambda x: x.YMax, extent_list)),
-                                          max(map(lambda x: x.YMin, extent_list))), output_spatial_reference)
+    return extent_to_polygon(arcpy.Extent(XMin=max(map(lambda x: x.XMin, extent_list)),
+                                          YMin=max(map(lambda x: x.YMin, extent_list)),
+                                          XMax=min(map(lambda x: x.XMax, extent_list)),
+                                          YMax=min(map(lambda x: x.YMax, extent_list))), output_spatial_reference)
 
 
 def calculate_output_extent_union(input_list, output_spatial_reference=None):
@@ -63,61 +69,37 @@ def calculate_output_extent_union(input_list, output_spatial_reference=None):
     :rtype: arcpy.Polygon
     """
     extent_list = [get_extent(i, output_spatial_reference) for i in input_list]
-    return extent_to_polygon(arcpy.Extent(max(map(lambda x: x.XMax, extent_list)),
-                                          min(map(lambda x: x.XMin, extent_list)),
-                                          max(map(lambda x: x.YMax, extent_list)),
-                                          min(map(lambda x: x.YMin, extent_list))), output_spatial_reference)
+    return extent_to_polygon(arcpy.Extent(XMin=min(map(lambda x: x.XMin, extent_list)),
+                                          YMin=min(map(lambda x: x.YMin, extent_list)),
+                                          XMax=max(map(lambda x: x.XMax, extent_list)),
+                                          YMax=max(map(lambda x: x.YMax, extent_list))), output_spatial_reference)
 
 
-def update_extent_with_cellsize_snapraster(polygon, cellsize, snapraster=None):
-    """
-    This function calculate the updated extent with cellsize and snapraster if provided
-    :type polygon: arcpy.Polygon
-    :type cellsize: float
-    :type snapraster: arcpy.sa.Raster
-    :rtype arcpy.Polygon
-    """
-    arcpy.CopyFeatures_management(polygon, "tmp.shp")
-    arcpy.env.cellSize = cellsize
-    if snapraster:
-        arcpy.env.snapRaster = snapraster
-    arcpy.PolygonToRaster_conversion("tmp.shp", "FID", "tmp.tif", "", "", cellsize)
-    arcpy.gp.clearEnvironment("cellSize")
-    arcpy.gp.clearEnvironment("snapRaster")
-    extent_raster = arcpy.sa.Raster("tmp.tif")
-    extent_polygon = extent_to_polygon(extent_raster.extent, extent_raster.spatialReference)
-    arcpy.Delete_management("tmp.shp")
-    arcpy.Delete_management("tmp.tif")
-    return extent_polygon
-
-
-def calculate_resolution_preserving_cellsize(input_ras, output_spatial_reference=None, extent=None):
+def calculate_resolution_preserving_cellsize(input_ras, output_spatial_reference, extent_shape):
     """
     Calculate resolution preserved cellsize when project raster to a different coordinate system
     :type input_ras: arcpy.sa.Raster
     :type output_spatial_reference: arcpy.SpatialReference
-    :type extent: arcpy.Extent
+    :type extent_shape: arcpy.Polygon
     :rtype float
     """
-    if not extent:
-        extent = input_ras.extent
     sr = input_ras.spatialReference
     cs_x = input_ras.meanCellWidth
     cs_y = input_ras.meanCellHeight
 
-    if not output_spatial_reference or sr == output_spatial_reference:
-        return cs_x, cs_y
+    if sr == output_spatial_reference:
+        return (cs_x + cs_y) / 2
 
-    polygon_old = extent_to_polygon(extent, sr)
-    polygon_new = polygon_old.densify("DISTANCE", cs_x, cs_x).projectAs(output_spatial_reference)
-    area_old = polygon_old.getArea("PLANAR", "SQUAREMETERS")
+    # polygon_old = extent_to_polygon(extent, sr)
+    # polygon_new = polygon_old.densify("DISTANCE", cs_x, cs_x).projectAs(output_spatial_reference)
+    polygon_new = extent_shape.projectAs(output_spatial_reference)
+    area_old = extent_shape.getArea("PLANAR", "SQUAREMETERS")
     area_new = polygon_new.getArea("PLANAR", "SQUAREMETERS")
     cs_new = math.sqrt((area_new / area_old) * cs_x * cs_y)
     return cs_new
 
 
-def raster_check(input_ras_list, env_ocs=None, env_cellsize=None, env_extent=None, env_snapraster=None,
-                 extent_type="intersect"):
+def raster_check(input_ras_list, env_ocs=None, env_cellsize=None, env_extent=None, env_snapraster=None):
     """
     Calculate correct cellsize with only one raster input, can be used to verify
     tools such as Abs, Lookup, Reclassify, Slice, etc.
@@ -135,38 +117,43 @@ def raster_check(input_ras_list, env_ocs=None, env_cellsize=None, env_extent=Non
         env_ocs = input_ras_list[0].spatialReference
 
     # Get env_extent and calculate output_extent
-    if env_extent is None:
-        if len(input_ras_list) == 1:
-            output_shape = extent_to_polygon(get_extent(input_ras_list[0], env_ocs), env_ocs)
-        else:
-            if extent_type == "union":
-                output_shape = calculate_output_extent_union(input_ras_list, env_ocs)
-            else:
-                output_shape = calculate_output_extent_intersect(input_ras_list, env_ocs)
+    if env_extent == "MAXOF":
+        output_shape = calculate_output_extent_union(input_ras_list, env_ocs)
+    elif env_extent == "MINOF":
+        output_shape = calculate_output_extent_intersect(input_ras_list, env_ocs)
     else:
-        # assuming user specified extent is in first input's spatial reference
-        output_shape = extent_to_polygon(env_extent, input_ras_list[0].spatialReference).projectAs(env_ocs)
+        output_shape = extent_to_polygon(env_extent.projectAs(env_ocs))
 
     # Calculate new cell size
-    if env_cellsize is None:  # Cell size is specified implicitly
+    if env_cellsize is None or env_cellsize == "MAXOF":  # Cell size is specified implicitly
         if env_snapraster is None:
             if len(input_ras_list) == 1:
-                cs_new = max(map(lambda x: calculate_resolution_preserving_cellsize(
-                    x, env_ocs, input_ras_list[0].extent), input_ras_list))
+                cs_new = max(map(lambda x: calculate_resolution_preserving_cellsize(x, env_ocs,
+                        extent_to_polygon(input_ras_list[0].extent, in_ras=input_ras_list[0])), input_ras_list))
             else:
                 cs_new = max(map(lambda x: calculate_resolution_preserving_cellsize(
-                    x, env_ocs, output_shape.projectAs(x.spatialReference).extent), input_ras_list))
+                    x, env_ocs, output_shape.projectAs(x.spatialReference)), input_ras_list))
         else:
             cs_new = max(map(lambda x: calculate_resolution_preserving_cellsize(x, env_ocs, env_snapraster.extent),
                              input_ras_list))
+    elif env_cellsize == "MINOF":  # Cell size is specified implicitly
+        if env_snapraster is None:
+            if len(input_ras_list) == 1:
+                cs_new = min(map(lambda x: calculate_resolution_preserving_cellsize(x, env_ocs,
+                        extent_to_polygon(input_ras_list[0].extent, in_ras=input_ras_list[0])), input_ras_list))
+            else:
+                cs_new = min(map(lambda x: calculate_resolution_preserving_cellsize(
+                    x, env_ocs, output_shape.projectAs(x.spatialReference)), input_ras_list))
+        else:
+            cs_new = min(map(lambda x: calculate_resolution_preserving_cellsize(x, env_ocs,
+                        extent_to_polygon(env_snapraster.extent, in_ras=env_snapraster)), input_ras_list))
     else:  # Cell size is specified explicitly
         if type(env_cellsize) == int or type(env_cellsize) == float:
             cs_new = env_cellsize
         else:
-            cs_new = calculate_resolution_preserving_cellsize(env_cellsize, env_ocs, env_cellsize.extent)
+            cs_new = calculate_resolution_preserving_cellsize(env_cellsize, env_ocs,
+                        extent_to_polygon(env_snapraster.extent, in_ras=env_snapraster))
 
-    # Calculate output boundary
-    # output_boundary = update_extent_with_cellsize_snapraster(output_shape, cs_new, env_snapraster)
     return cs_new, output_shape
 
 
@@ -190,7 +177,7 @@ def feature_check(input_fc, param_cellsize=None, env_ocs=None, env_cellsize=None
     # Get env_extent and calculate output_extent
     if env_extent is None:
         env_extent = arcpy.Describe(input_fc).extent
-    output_shape = extent_to_polygon(get_extent(input_fc, env_ocs), env_ocs)
+    output_shape = extent_to_polygon(env_extent.projectAs(env_ocs))
 
     # Calculate new cell size
     if param_cellsize is None:  # Parameter cell size is specified implicitly
@@ -209,20 +196,21 @@ def feature_check(input_fc, param_cellsize=None, env_ocs=None, env_cellsize=None
                              / 250
                     arcpy.gp.clearEnvironment("extent")
             else:
-                cs_new = calculate_resolution_preserving_cellsize(env_snapraster, env_ocs, env_snapraster.extent)
+                cs_new = calculate_resolution_preserving_cellsize(env_snapraster, env_ocs,
+                            extent_to_polygon(env_snapraster.extent, in_ras=env_snapraster))
         else:  # Environment cell size is specified explicitly
             if type(env_cellsize) == int or type(env_cellsize) == float:
                 cs_new = env_cellsize
             else:
-                cs_new = calculate_resolution_preserving_cellsize(env_cellsize, env_ocs, env_cellsize.extent)
+                cs_new = calculate_resolution_preserving_cellsize(env_cellsize, env_ocs,
+                            extent_to_polygon(env_cellsize.extent, in_ras=env_cellsize))
     else:  # Parameter cell size is specified explicitly
         if type(param_cellsize) == int or type(param_cellsize) == float:
             cs_new = param_cellsize
         else:
-            cs_new = calculate_resolution_preserving_cellsize(param_cellsize, env_ocs, param_cellsize.extent)
+            cs_new = calculate_resolution_preserving_cellsize(param_cellsize, env_ocs,
+                        extent_to_polygon(param_cellsize.extent, in_ras=param_cellsize))
 
-    # Calculate output boundary
-    # output_boundary = update_extent_with_cellsize_snapraster(output_shape, cs_new, env_snapraster)
     return cs_new, output_shape
 
 
@@ -267,8 +255,8 @@ def euclidean_distance_check(param_input, param_cellsize=None, env_ocs=None, env
     """
     if type(param_input) == arcpy.sa.Raster:
         if param_cellsize:
-            return raster_check([param_input], env_ocs, param_cellsize, env_extent, env_snapraster, "intersect")
-        return raster_check([param_input], env_ocs, env_cellsize, env_extent, env_snapraster, "intersect")
+            return raster_check([param_input], env_ocs, param_cellsize, env_extent, env_snapraster)
+        return raster_check([param_input], env_ocs, env_cellsize, env_extent, env_snapraster)
     else:
         return feature_check(param_input, param_cellsize, env_ocs, env_cellsize, env_extent, env_snapraster)
 
